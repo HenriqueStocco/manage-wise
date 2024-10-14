b1c66c61f933ea17549d3b2e74bdf4c2ea93c276
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-14 13:15:28 -0300

    base

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ export function App() {
   return (
     // bg-gradient-to-r from-teal-950 to-teal-700
     <main className="bg-teal-950">
-      <section className=""></section>
+      <section className="">
+        <h1>OPA</h1>
+      </section>
     </main>
   )
 }
