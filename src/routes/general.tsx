8c5368f73db1c4c92b23057fffaeff734aa139ec
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-16 13:25:57 -0300

    feat: general dashboard page, entry page

---
+++ src/routes/general.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "../components/sidebar"
+
+function GeneralPage() {
+  return (
+    <div className="flex w-full">
+      <Sidebar />
+    </div>
+  )
+}
+
+export { GeneralPage }
