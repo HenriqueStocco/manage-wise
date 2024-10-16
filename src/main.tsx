9db8f15b7d9e4ac17c58af3185d4fff52b5afa15
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-15 22:37:44 -0300

    idk what place in this commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
 import './styles/globals.css'
+import { LoginPage } from './routes/login'
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>,
 )
