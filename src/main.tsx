f65e236655552025c7754c1a70da71dcb8b2c911
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-15 22:45:09 -0300

    fix: add miracles space  between imports and might half component root

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import { LoginPage } from './routes/login'
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
