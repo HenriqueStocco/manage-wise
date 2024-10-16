0d3b878c5d78086cd0c082e8138e4dca4e86a726
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-16 13:24:42 -0300

    new page being builded

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
-import { LoginPage } from './routes/login'
+import { GeneralPage } from './routes/general'
+// import { LoginPage } from './routes/login'
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
+    <GeneralPage />
   </StrictMode>,
 )
